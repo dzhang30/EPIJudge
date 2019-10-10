852d3f490966e04871c5c25a2828288d7c59d5f0
Daniel Zhang <dzhanner17@gmail.com>
2019-10-09 20:37:43 -0400

    10/8

--- epi_judge_python/spiral_ordering_segments.py
+++ epi_judge_python/spiral_ordering_segments.py
@@ -1,21 +1,38 @@
 from test_framework import generic_test
+from pprint import pprint
 
 
 def matrix_in_spiral_order(square_matrix):
     # TODO - you fill in here.
-    length = len(square_matrix[0])
-    width = length - 1
     result = []
-    flip = False
-    while length > 0 or width > 0:
-        for i in range(length):
-            for j in range(width):
-                result.append()
+    length = len(square_matrix)
+    if not length:
+        return result
+    # if length == 1:
+    #     return square_matrix[0]
+    shift = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    direction = 0
+    x = 0
+    y = 0
+    for _ in range(length ** 2):
+        result.append(square_matrix[x][y])
+        square_matrix[x][y] = 0
+        dir_x = x + shift[direction][0]
+        dir_y = y + shift[direction][1]
+        if not (0 <= dir_x < length and 0 <= dir_y < length and square_matrix[dir_x][dir_y] != 0):
+            direction = 0 if direction == 3 else direction + 1
+            x = x + shift[direction][0]
+            y = y + shift[direction][1]
+        else:
+            x = dir_x
+            y = dir_y
 
-    return []
+    return result
 
 
 if __name__ == '__main__':
+    # square_matrix = [[1, 4, 7], [9, 8, 2], [3, 5, 6]]
+    # matrix_in_spiral_order(square_matrix)
     exit(
         generic_test.generic_test_main("spiral_ordering_segments.py",
                                        "spiral_ordering_segments.tsv",
